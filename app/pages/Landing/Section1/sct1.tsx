dc1d2eb39b59ae1b00735948b4b1aafa027d879c
Rovshan Alizada <148777319+rovshanalizada7@users.noreply.github.com>
2025-06-26 14:03:37 +0400

    general design updated

--- app/pages/Landing/Section1/sct1.tsx
+++ app/pages/Landing/Section1/sct1.tsx
@@ -28,9 +28,24 @@ export const Section1 = () => {
                 </menu>
             </header>
 
-            {/* <div>
+            <div
+                className="bg-gradient-to-r w-[350px] h-[150px] absolute z-2 left-[15%] bottom-[-3%] rounded-[50%] blur-[220px]"
+                style={{
+                    backgroundImage: 'linear-gradient(to right, rgba(252, 111, 50, 1), rgba(255, 74, 89, 1))',
+                }}
+            ></div>
+
+             <div
+                className="bg-gradient-to-r absolute z-2 left-[23%] top-[-10%] w-[350px] h-[250px] blur-[200px] rounded-[50%]"
+                style={{
+                    backgroundImage: 'linear-gradient(to right, rgba(50, 191, 252, 1), rgba(0, 85, 119, 1))',
+                }}
+             ></div>
+
+
+            <div>
                 <img className="w-[1016px] h-[1016px]  right-0 top-[5%] z-1 fixed" src="imgs/moon.jpg" alt="" />
-            </div> */}
+            </div>
 
             <div className="flex flex-col gap-[100px]  justify-between h-[784p]  w-[92%] absolute z-[2]">
 
--- components/Table/table.tsx
+++ components/Table/table.tsx
@@ -8,8 +8,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import "./table.css"
-
+import "./table.css";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -21,29 +20,26 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
+const StyledTableRow = styled(TableRow)(() => ({
+  // Remove MUI's default odd-row styling so our CSS can apply
   '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
 
-function createData(name:any, calories:any, fat:any, carbs:any, protein:any) {
+function createData(name: any, calories: any, fat: any, carbs: any, protein: any) {
   return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-   createData('Frozen yoght', 159, 6.0, 24, 4.0),
-  createData('Ice crem sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclir', 262, 16.0, 24, 6.0),
+  createData('- 1 davidkim205/Rhea-72b-v0.5', 81.22, 79.78, 91.15, 77.95),
+  createData(' 2 MTSAIR/MultiVerse_70B', 237, 9.0, 37, 4.3),
+  createData('3 SF-Foundation/Ein-72B-v0.11', 262, 16.0, 24, 6.0),
+  createData('4 abacusai/Smaug-72B-v0.1', 305, 3.7, 67, 4.3),
+  createData('5 ibivibiv/alpaca-dragon-72b-v1', 356, 16.0, 49, 3.9),
+  createData('6 mistralai/Mixtral-8x22B-Instruct-v0.1', 159, 6.0, 24, 4.0),
+  createData('7 moreh/MoMo-72B-lora-1.8.7-DPO', 237, 9.0, 37, 4.3),
+  createData('8 cloudyu/TomGrc_FusionNet_34Bx2_MoE_', 262, 16.0, 24, 6.0),
 ];
 
 export default function CustomizedTables() {
@@ -52,24 +48,27 @@ export default function CustomizedTables() {
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Prein&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Proin&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Proin&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Proin&nbsp;(g)</StyledTableCell>
+            <StyledTableCell># Model Name</StyledTableCell>
+            <StyledTableCell align="right">Average</StyledTableCell>
+            <StyledTableCell align="right">ARC</StyledTableCell>
+            <StyledTableCell align="right">HellaSwag</StyledTableCell>
+            <StyledTableCell align="right">MMLU</StyledTableCell>
+            <StyledTableCell align="right">TruthfulQA</StyledTableCell>
+            <StyledTableCell align="right">Winogrande</StyledTableCell>
+            <StyledTableCell align="right">GSM8K</StyledTableCell>
+            <StyledTableCell align="right">Usage</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name} className='bg-[gray] '>
+          {rows.map((row, index) => (
+            <StyledTableRow
+              key={row.name}
+              className={index % 2 === 0 ? 'odd-rows' : 'even-rows'}
+            >
               <StyledTableCell component="th" scope="row">
                 {row.name}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right" >{row.calories}</StyledTableCell>
               <StyledTableCell align="right">{row.fat}</StyledTableCell>
               <StyledTableCell align="right">{row.carbs}</StyledTableCell>
               <StyledTableCell align="right">{row.protein}</StyledTableCell>
